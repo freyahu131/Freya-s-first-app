54eddb89a20406e22495e34688321ed28a86db15
mac <mac@macde-MacBook-Pro.local>
2017-07-17 18:59:11 +0800

    Initial Commit

---
+++ Freya's first app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Freya's first app
+//
+//  Created by mac on 2017/7/17.
+//  Copyright © 2017年 Justwithin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
